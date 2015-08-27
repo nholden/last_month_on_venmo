4c6c65a23775fe8ebd7023c13884e918aa59b5f3
Nick Holden <nholden@bu.edu>
2015-08-27 07:17:09 -0400

    Generalize LastMonthsPayments class to Payments which initializes with an optional past_days parameter (default is 30)

--- last_month_on_venmo.rb
+++ last_month_on_venmo.rb
@@ -2,10 +2,10 @@ require "json"
 require "net/http"
 require "date"
 
-class LastMonthPayments
-  def initialize(access_token)
-    thirty_days_ago = ((Date.today)-30).to_s
-    url = "https://api.venmo.com/v1/payments?access_token=#{access_token}&after=#{thirty_days_ago}"
+class Payments
+  def initialize(access_token, past_days = 30)
+    after_date = ((Date.today)-past_days).to_s
+    url = "https://api.venmo.com/v1/payments?access_token=#{access_token}&after=#{after_date}"
     response = Net::HTTP.get_response(URI.parse(url))
     data = response.body
     @payments = JSON.parse(data)["data"]
@@ -16,11 +16,11 @@ class LastMonthPayments
     @payments.each do |payment|
       if payment["target"]["user"]["username"].downcase == username.downcase or 
          payment["actor"]["username"].downcase == username.downcase
-        date = payment['date_created'][0,10]
-        actor = payment['actor']['first_name']
-        target = payment['target']['user']['first_name']
-        amount = '%.2f' % payment['amount']
-        note = payment['note']
+        date = payment["date_created"][0,10]
+        actor = payment["actor"]["first_name"]
+        target = payment["target"]["user"]["first_name"]
+        amount = "%.2f" % payment["amount"]
+        note = payment["note"]
         payments_with_user << "On #{date}, #{actor} paid #{target} $#{amount} for #{note}"
       end
     end
@@ -33,7 +33,7 @@ access_token = gets.chomp
 print "Other person's username: "
 username = gets.chomp
 
-last_month_payments = LastMonthPayments.new(access_token)
+last_month_payments = Payments.new(access_token)
 last_month_payments_with_user = last_month_payments.with_user(username)
 if last_month_payments_with_user.empty?
   puts "No payments with #{username} found."
