e13ff600827a33b972b4c4ca0b8489865199a934
Nick Holden <nholden@bu.edu>
2015-08-26 21:09:31 -0400

    Remove unnecessary methods and rename variables to be more intuitive

--- last_month_on_venmo.rb
+++ last_month_on_venmo.rb
@@ -2,12 +2,9 @@ require "json"
 require "net/http"
 require "date"
 
-class RecentPayments
-  attr_accessor :payments
-
+class LastMonthPayments
   def initialize(access_token)
-    today = Date.today
-    thirty_days_ago = (today-30).to_s
+    thirty_days_ago = ((Date.today)-30).to_s
     url = "https://api.venmo.com/v1/payments?access_token=#{access_token}&after=#{thirty_days_ago}"
     response = Net::HTTP.get_response(URI.parse(url))
     data = response.body
@@ -16,25 +13,17 @@ class RecentPayments
 
   def with_user(user_id)
     payments_with_user = []
-    payments.each do |payment|
+    @payments.each do |payment|
       if payment["target"]["user"]["id"] == user_id or payment["actor"]["id"] == user_id
-        payments_with_user << payment
+        date = payment['date_created'][0,10]
+        actor = payment['actor']['first_name']
+        target = payment['target']['user']['first_name']
+        amount = '%.2f' % payment['amount']
+        note = payment['note']
+        payments_with_user << "On #{date}, #{actor} paid #{target} $#{amount} for #{note}"
       end
     end
     payments_with_user
-  end
-
-  def summaries_with_user(user_id)
-    payment_summaries_with_user = []
-    with_user(user_id).each do |payment|
-      date = payment['date_created'][0,10]
-      actor = payment['actor']['first_name']
-      target = payment['target']['user']['first_name']
-      amount = '%.2f' % payment['amount']
-      note = payment['note']
-      payment_summaries_with_user << "On #{date}, #{actor} paid #{target} $#{amount} for #{note}"
-    end
-    payment_summaries_with_user
   end     
 end
 
@@ -43,4 +32,5 @@ access_token = gets.chomp
 print "Other user's ID: "
 user_id = gets.chomp
 
-RecentPayments.new(access_token).summaries_with_user(user_id).each { |summary| puts summary }
+@user_last_month_payments = LastMonthPayments.new(access_token)
+@user_last_month_payments.with_user(user_id).each { |payment| puts payment }
