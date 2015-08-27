608142815582a7b2eb7bcae30cf7f607df244895
Nick Holden <nholden@bu.edu>
2015-08-26 20:16:12 -0400

    Add with_user method

--- last_month_on_venmo.rb
+++ last_month_on_venmo.rb
@@ -1,14 +1,23 @@
 require "json"
 require "net/http"
 
-print "Access token: "
-access_token = gets.chomp
+class RecentPayments
+  attr_accessor :payments
 
-def recent_payments(access_token)
-  url = "https://api.venmo.com/v1/payments?access_token=#{access_token}"
-  response = Net::HTTP.get_response(URI.parse(url))
-  data = response.body
-  JSON.parse(data)
+  def initialize(access_token)
+    url = "https://api.venmo.com/v1/payments?access_token=#{access_token}"
+    response = Net::HTTP.get_response(URI.parse(url))
+    data = response.body
+    @payments = JSON.parse(data)["data"]
+  end
+
+  def with_user(user_id)
+    payments_with_user = []
+    self.payments.each do |payment|
+      if payment["target"]["user"]["id"] == user_id or payment["actor"]["id"] == user_id
+        payments_with_user << payment
+      end
+    end
+    payments_with_user
+  end
 end
-
-puts recent_payments(access_token)
